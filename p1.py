18f27e0965a5644f73dfd9540313b037bb93484f
user <bxl190001@utdallas.edu>
2022-10-23 10:18:07 -0500

    create min cost network, write graphs and figures to files

--- p1.py
+++ p1.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import matplotlib.pyplot as plt
+import networkx as nx
 import numpy as np
 
 N = 21 # number of nodes
@@ -6,37 +8,88 @@ id = '648274120264827412026'
 test_min_r = 3
 test_max_r = 14 + 1
 
-def min_net(costs, min_flow):
-    for i in range(costs.shape[0]): 
-        np.minimum(costs, costs[:, i, np.newaxis] + costs[i], out=costs)
-        
+# module 2
+def min_net(costs, demands, net):
+    min_paths = np.empty((N, N), dtype=object)
 
-def init(costs, min_flow, r):
-    for i in range(len(id)):
-        for i2 in range(len(id)):
-            min_flow[i][i2] = int(id[i]) - int(id[i2])
+    for i in range(N):
+        for i2 in range(N):
+            min_paths[i][i2] = []
 
-    rng = np.random.default_rng()
-    costs.fill(100)
-    rand_set = tuple(range(costs.shape[0]))
+    for i in range(N): 
+        for i2 in range(N):
+            for i3 in range(N):
+                min_cost_i = costs[i2][i] + costs[i][i3]
+                if costs[i2][i3] > min_cost_i:
+                    min_paths[i2][i3] = min_paths[i2][i] + [i] + min_paths[i][i3]
+                    costs[i2][i3] = min_cost_i
 
-    for i in range(costs.shape[0]):
-        indices = rng.choice(rand_set[:i] + rand_set[i + 1:], r, replace=False)
-        costs[i][indices] = 1
+    for i in range(N):
+        for i2 in range(N):
+            min_path = min_paths[i][i2]
+            min_path.append(i2)
+            for i3 in range(len(min_path) - 1): 
+                edge = (min_path[i3], min_path[i3 + 1])
+                net[edge] += demands[i][i2]
 
-def show_net(net):
-    print(net)
+# module 3
+def show(densities, net, total_costs):
+    plt.plot(total_costs)
+    plt.xlabel('k')
+    plt.ylabel('Total Cost')
+    plt.savefig('costs.png')
+    plt.clf()
+
+    plt.plot(densities)
+    plt.xlabel('k')
+    plt.ylabel('Density')
+    plt.savefig('densities.png')
+    plt.clf()
+
+    plt.figure(figsize=(20, 20))
+
+    for i in [3, 8, 14]:
+        G = nx.from_numpy_matrix(net[i])
+        #layout = nx.circular_layout(G)
+        layout = nx.shell_layout(G)
+        nx.draw(G, layout, with_labels=True)
+        edge_labels = nx.get_edge_attributes(G, 'weight')
+        nx.draw_networkx_edge_labels(G, pos=layout, edge_labels=edge_labels)
+        plt.savefig(f'graph{i}.png')
+        plt.clf()
 
 def main():
-    min_flow = np.empty(shape=(N, N), dtype=int)
-    costs = np.empty(shape=(N, N), dtype=int)
+    demands = np.empty((N, N), dtype=int)
+    costs = np.full((N, N), 100)
+    net = np.zeros((N, N, N), dtype=int)
+    total_costs = np.zeros(N, dtype=int)
+    densities = np.zeros(N)
+
+    # module 1
+    for i in range(len(id)):
+        for i2 in range(len(id)):
+            demands[i][i2] = max(int(id[i]) - int(id[i2]), 0)
+
+    rng = np.random.default_rng()
+    rand_set = tuple(range(N))
+    num_dir_edges = N * (N - 1)
 
     for r in range(test_min_r, test_max_r):
-        init(costs, min_flow, r)
-        print(costs)
-        print(min_flow)
-        net = min_net(costs, min_flow)
-        show_net(net)
+        costs.fill(100)
+        np.fill_diagonal(costs, 0)
+
+        for i in range(N):
+            indices = rng.choice(rand_set[:i] + rand_set[i + 1:], r, replace=False)
+            costs[i][indices] = 1
+
+        #print(costs)
+        #print(demands)
+
+        min_net(costs, demands, net[r])
+        total_costs[r] = np.sum(demands * costs)
+        densities[r] =  float(np.count_nonzero(net[r])) / num_dir_edges
+
+    show(densities, net, total_costs)
 
 if __name__ == '__main__':
     main()
